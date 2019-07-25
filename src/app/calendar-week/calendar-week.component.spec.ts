f518ba78f3175134d8400c0e2c96bf7b662355a1
Michal Martinek <martinek8@gmail.com>
2019-07-25 20:50:29 +0200

    test: add basic tests for calendar-week

--- src/app/calendar-week/calendar-week.component.spec.ts
+++ src/app/calendar-week/calendar-week.component.spec.ts
@@ -1,25 +1,76 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { Component } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { ModalModule } from 'ngx-bootstrap/modal';
 
+import { CalendarItemComponent } from '../calendar-item/calendar-item.component';
+import { IssueLabelComponent } from '../issue-label/issue-label.component';
+import { DailyTimeEntryComponent } from '../daily-time-entry/daily-time-entry.component';
+import { GapComponent } from '../gap/gap.component';
+import { TimeEntriesComponent } from '../time-entries/time-entries.component';
+import { weekLog1 } from '../models/time-entries.mock';
 import { CalendarWeekComponent } from './calendar-week.component';
 
+
 describe('CalendarWeekComponent', () => {
-  let component: CalendarWeekComponent;
-  let fixture: ComponentFixture<CalendarWeekComponent>;
+  @Component({
+    selector: `app-host-component`,
+    template: `<app-calendar-week [weekLog]="weekLog"></app-calendar-week>`
+  })
+  class TestHostComponent {
+    weekLog = weekLog1;
+  }
+
+  let component: TestHostComponent;
+  let fixture: ComponentFixture<TestHostComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CalendarWeekComponent ]
+      declarations: [
+        TestHostComponent,
+        CalendarWeekComponent,
+        CalendarItemComponent,
+        IssueLabelComponent,
+        GapComponent,
+        DailyTimeEntryComponent,
+        TimeEntriesComponent
+      ],
+      imports: [
+        HttpClientModule,
+        ReactiveFormsModule,
+        ModalModule.forRoot()
+      ],
     })
-    .compileComponents();
+      .compileComponents();
+
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(CalendarWeekComponent);
+    fixture = TestBed.createComponent(TestHostComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display calendar-items', () => {
+    const calendarWeekElement: HTMLElement = fixture.nativeElement;
+    const elements = calendarWeekElement.querySelectorAll('app-calendar-item');
+    expect(elements.length).toEqual(weekLog1.dayLogs.length);
+  });
+
+  it('should display containers for missing days', () => {
+    const calendarWeekElement: HTMLElement = fixture.nativeElement;
+    const elements = calendarWeekElement.querySelectorAll('.missing');
+    expect(elements.length).toEqual(7 - weekLog1.dayLogs.length);
+  });
+
+  it('should display week number', () => {
+    const calendarWeekElement: HTMLElement = fixture.nativeElement;
+    const weekNumberElement = calendarWeekElement.querySelector('.card-text');
+    expect(weekNumberElement.textContent).toContain(`${weekLog1.weekNumber}.`);
   });
 });
