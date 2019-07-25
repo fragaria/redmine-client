a908c6e00d1f106a51d51ec0d035c56e230bf5d7
Michal Martinek <martinek8@gmail.com>
2019-07-25 20:49:28 +0200

    test: fix code style and add testing data for week log

--- src/app/models/time-entries.mock.ts
+++ src/app/models/time-entries.mock.ts
@@ -1,65 +1,74 @@
-import { DayLog } from './time-entries';
+import { DayLog, WeekLog } from './time-entries';
 
 
 export const dayLog1: DayLog = {
-    date: "2019-07-22", dayOfWeek: 1, timeEntries: {
+    date: '2019-07-22', dayOfWeek: 1, timeEntries: {
         total_count: 0,
         offset: 0,
         limit: 0,
         time_entries: [{
-            activity: { id: 9, name: "Development" },
-            comments: "a",
-            created_on: "2019-07-17T08:48:35Z",
+            activity: { id: 9, name: 'Development' },
+            comments: 'a',
+            created_on: '2019-07-17T08:48:35Z',
             hours: 8,
             id: 29329,
             isNew: true,
             issue: { id: 4411 },
-            project: { id: 112, name: "Redmine test" },
-            spent_on: "2019-07-22",
-            updated_on: "2019-07-17T08:48:35Z",
-            user: { id: 56, name: "John Doe" }
+            project: { id: 112, name: 'Redmine test' },
+            spent_on: '2019-07-22',
+            updated_on: '2019-07-17T08:48:35Z',
+            user: { id: 56, name: 'John Doe' }
         }]
     }, hoursLogged: 8
 };
 
 export const dayLog2: DayLog = {
-    date: "2019-07-22", dayOfWeek: 1, timeEntries: {
+    date: '2019-07-22', dayOfWeek: 1, timeEntries: {
         total_count: 0,
         offset: 0,
         limit: 0,
         time_entries: [{
-            activity: { id: 9, name: "Development" },
-            comments: "a",
-            created_on: "2019-07-17T08:48:35Z",
+            activity: { id: 9, name: 'Development' },
+            comments: 'a',
+            created_on: '2019-07-17T08:48:35Z',
             hours: 8.2,
             id: 29329,
             isNew: true,
             issue: { id: 4411 },
-            project: { id: 112, name: "Redmine test" },
-            spent_on: "2019-07-22",
-            updated_on: "2019-07-17T08:48:35Z",
-            user: { id: 56, name: "John Doe" }
+            project: { id: 112, name: 'Redmine test' },
+            spent_on: '2019-07-22',
+            updated_on: '2019-07-17T08:48:35Z',
+            user: { id: 56, name: 'John Doe' }
         }, {
-            activity: { id: 9, name: "Development" },
-            comments: "a",
-            created_on: "2019-07-17T08:48:35Z",
+            activity: { id: 9, name: 'Development' },
+            comments: 'a',
+            created_on: '2019-07-17T08:48:35Z',
             hours: 2.05,
             id: 29329,
             isNew: true,
             issue: { id: 4411 },
-            project: { id: 112, name: "Redmine test" },
-            spent_on: "2019-07-22",
-            updated_on: "2019-07-17T08:48:35Z",
-            user: { id: 56, name: "John Doe" }
+            project: { id: 112, name: 'Redmine test' },
+            spent_on: '2019-07-22',
+            updated_on: '2019-07-17T08:48:35Z',
+            user: { id: 56, name: 'John Doe' }
         }]
     }, hoursLogged: 10.25
 };
 
 export const emptyDayLog: DayLog = {
-    date: "2019-07-22", dayOfWeek: 1, timeEntries: {
+    date: '2019-07-22', dayOfWeek: 1, timeEntries: {
         total_count: 0,
         offset: 0,
         limit: 0,
         time_entries: []
     }, hoursLogged: 0
-}
+};
+
+export const weekLog1: WeekLog = {
+    weekNumber: 27,
+    startsWith: 1,
+    numberOfWorkingDays: 5,
+    numberOfDays: 3,
+    hoursLogged: 18.25,
+    dayLogs: [dayLog1, dayLog2, emptyDayLog]
+};
